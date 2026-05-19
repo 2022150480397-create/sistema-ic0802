2faff449f0a143d5ba1c561148c6b02667c6771a
Karlita Gerardo <karlitagerardo@MacBook-de-Karlita.local>
2026-05-18 19:24:26 -0600

    cambios home

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -59,7 +59,7 @@ export default function HomePage() {
                 >
                   BIENVENIDAS
                   <br />
-                  Nuestro sistema 
+                  Mi sistema 💕
                 </Typography>
 
                 <Typography
